260edfc1ac7e5d568bf21e391533603c31f155ee
Tararatel <caratel18@mail.ru>
2021-05-24 17:18:52 +0300

    Update code

--- Lesson_1/js/main.js
+++ Lesson_1/js/main.js
@@ -5,34 +5,25 @@ const products = [
     {id: 2, title: 'Mouse', price: 20, poster: 'images/mouse.png'},
     {id: 3, title: 'Keyboard', price: 200, poster: 'images/keyboard.png'},
     {id: 4, title: 'Gamepad', price: 50, poster: 'images/gamepad.png'},
-    {id: 5, title: 'Mousepad', price: '', poster: 'images/mousepad.png'},
-    {id: 6, title: 'Headset', price: '', poster: ''},
+    {id: 5, title: 'Mousepad', poster: 'images/mousepad.png'},
+    {id: 6, title: 'Headset'},
 ];
 //Функция для формирования верстки каждого товара
-const renderProduct = item => {
-    if(item.price == '') {
-        item.price = 'Нет в наличии';   
-    }
-
-    if(item.poster == '') {
-        item.poster = 'images/no_photo.png';
-    } 
+const renderProduct = (title = 'Новый арт.', price = 'Товара нет в наличии', poster = 'images/no_photo.png') => {
 
     return `<div class="product-item">
-                <h3>${item.title}</h3>
-                <img src="${item.poster}" alt="${item.title}">                                
-                <p>Price: ${item.price}</p>
+                <h3>${title}</h3>
+                <img src="${poster}" alt="${title}">                                
+                <p>Price: ${price}</p>
                 <button class="buy-btn">Купить</button>
             </div>`;
 };
 
 const renderPage = (list) => {
-    const productsList = list.map(item => renderProduct(item));
-    console.log(productsList);
-    document.querySelector('.products').innerHTML = productsList.join("");
+    document.querySelector('.products').innerHTML = list.map(item => renderProduct(item.title, item.price, item.poster)).join("");
     
     
-    // Запятые появлялись, потому что мы выводили не элементы разметки отдельно, а массив целиком, который перечислял элементы через запятую.
+    // Запятые появлялись, потому что мы выводили не элементы разметки отдельно, а массив целиком, который перечислял элементы через запятую. Метод join() даёт возможность выбрать нужный разделитель. В данном случае мы его не указываем и запятые исчезают.
 };
 
 renderPage(products);
